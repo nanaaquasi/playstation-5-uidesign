a9c68d1c82a779289dafb39e666615f3de2d8f32
Nana Aquasi <nanaaquasi5@gmail.com>
2020-11-30 20:51:01 +0000

    initial commit

---
+++ src/utils.js
@@ -0,0 +1,12 @@
+// import React, { useState, useEffect } from "react";
+
+// export const useAudio = (url) => {
+//   const [audio] = useState(new Audio(url));
+//   const [playing, setPlaying] = useState(false);
+
+//   const toggle = () => setPlaying(!playing);
+
+//   useEffect(() => {
+//     playing ? audio.play() : audio.pause();
+//   }, [playing]);
+// };
